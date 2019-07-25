75900794378495f03be660f88edf13ab9effa615
Bala Raman <vrbmails@gmail.com>
2019-07-25 22:11:41 +0200

    added admin (@todo) endpoint to set up sample products in the system

--- ultraboost/src/main/java/com/bala/products/controller/v1/ProductController.java
+++ ultraboost/src/main/java/com/bala/products/controller/v1/ProductController.java
@@ -1,7 +1,7 @@
 package com.bala.products.controller.v1;
 
 
-import com.bala.products.dto.Product;
+import com.bala.products.dto.*;
 import com.bala.products.service.KafkaProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +17,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping(value = "/api/v1", produces = "application/json")
@@ -76,6 +78,51 @@ public class ProductController {
                 return new ResponseEntity<>("Success", HttpStatus.OK);
             }
         }
+    }
+
+    // TODO: should be admin only endpoint.
+    @RequestMapping(value = "/sample", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<String> setupSampleProducts() {
+
+        MetaData md = new MetaData()
+                .setCanonical("//www.adidas.co.uk/nite-jogger-shoes/product.html")
+                .setDescription("Shop for Nite Jogger Shoes - Black at adidas.co.uk! See all the styles " +
+                        "and colours of Nite Jogger Shoes - Black at the official adidas UK online store.")
+                .setKeywords("Nite Jogger Shoes")
+                .setSiteName("adidas United Kingdom")
+                .setPageTitle("adidas Nite Jogger Shoes - Black | adidas UK");
+
+        PricingInformation pi = new PricingInformation()
+                .setCurrentPrice(119.95)
+                .setStandardPrice(199.95)
+                .setStandardPriceNoVat(119.95);
+
+        ProductDescription pd = new ProductDescription()
+                .setSubtitle("Modern cushioning updates this flashy'80s standout.")
+                .setTitle("Nite Jogger Shoes")
+                .setText("Inspired by the 1980 Nite Jogger, these shoes shine bright with retro style and " +
+                        " reflective details. The mesh and nylon ripstop upper is detailed with suede overlays. An " +
+                        " updated Boost midsole adds responsive cushioning for all-day comfort.");
+
+        List<Integer> range = IntStream.range(10, 91).boxed().collect(Collectors.toList());
+        for (int i : range) {
+            String pId = String.format("CG70%d", i);
+            String modelNumber = String.format("BT0%d", i);
+
+            Product p = new Product()
+                    .setId(pId)
+                    .setModelNumber(modelNumber)
+                    .setName("Nite Jogger Lite")
+                    .setProductType("inline")
+                    .setMetaData(md)
+                    .setPricingInformation(pi)
+                    .setProductDescription(pd);
+
+            kafkaProducer.sendMessage(p);
+        }
+
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
 }
